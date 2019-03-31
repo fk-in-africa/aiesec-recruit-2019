38e50e2fa4b70e64cfb83062ef1ecc8065e788e4
yuki komatsu <yuki.k.lion@gmail.com>
2019-03-31 14:05:11 +0900

    delete:Renew_aiesecrecruit2018

--- Renew_aiesecrecruit2018/assets/js/video.js
+++
@@ -1,8 +0,0 @@
-if( window.matchMedia('(max-width:992px)').matches ){
-	var video = document.getElementById("BackMovie");
-	video.pause();
-	video.currentTime = 0;
-
-	video.style.display="none";
-	document.getElementById('TopBg').className = "afterVideo";
-}
